6c6178054547cdc12555f4991d515cbc7302f429
hamed9980 <hamedbelkhiria980@gmail.com>
2022-08-08 11:25:46 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import WallPaper from '../images/OIP.jpg'
+export default function Main(){
+  return (
+    <div>
+    <img src={WallPaper}/>
+    Hello world
+    </div>
+  )
+}
