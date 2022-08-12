980dfaa1bb89ea9b56bcc5263907dfe075a301ba
hamed9980 <hamedbelkhiria980@gmail.com>
2022-08-12 18:37:19 +0200

    hook change2

--- src/App.js
+++ src/App.js
@@ -2,32 +2,71 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main'
 import tableau from './tableau'
-import React,{useState,useEffect} from 'react'
-function App() {
-  var tableau1=tableau;
-  const [tab1,changeColor]=useState(tableau1);
+import React,{useEffect} from 'react'
+import useState from 'react-usestateref'
+function AppComponentRenderer({color1,style,value,onClick}){
+
+  return (<button onClick={onClick} style={{color:color1?'gray':'blue'}}
+
+
+      >{value} {color1?'gray':'blue'}</button>)
+}
+function App({onClick})  {
+
+
+  var tableau2=tableau;
+  var [tab1,changeColor,ref]=useState(tableau);
+useEffect(()=>{
+  console.log('Color updated')
+},[ref.current])
   function changeColor1(value){
-
-
+console.log(value)
   changeColor(
     prevAtt=>{
       prevAtt[value].color=!prevAtt[value].color;
-      console.log(tableau1[value],tab1[value])
+console.log(prevAtt[value])
         return prevAtt;
+
     })
 
   }
-  useEffect(()=>{
-    console.log('effect')
-
-  },[tab1])
 return(<div>Hello
-  {tab1.map((x)=>(
-    <button style={{color:x.color?'gray':'blue'}}
-
-     key={x.key}
-      onClick={()=>changeColor1(x.key)}>{x.value} {x.color?'gray':'blue'}</button>
+  {ref.current.map((x)=>(
+  <AppComponentRenderer onClick={()=>{changeColor1(x.key);onClick();}} key={x.key} key1={x.key} value={x.value}  color1={x.color}/>
   ))}
 </div>)
 }
-export default App
+
+
+function shouldComponentUpdate(nextProps, nextState){
+if (this.state.data !== nextState.data){
+  this.setState({
+    data: nextState.data
+  })
+  return true
+}
+return false
+}
+function App1(){
+  const [écrire,setécrire]=useState("")
+  function changé(évenement){
+    console.log(évenement.target.value)
+  }
+  return (<div><form>
+  <input type="text"
+placeholder="écrire"
+onChange={changé}
+name="ecrire"/>
+</form>
+</div>
+)
+}
+class App2 extends React.Component{
+  render(){
+  return (<App onClick={this.force}/>)
+}
+force=()=>{
+  this.forceUpdate()
+}
+}
+export default App2
